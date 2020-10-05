b47eaca4aa4ece51c777b71ff79bd27a22296906
abdo <abdo45010@gmail.com>
2020-10-05 20:54:37 +0200

    final commit

--- todolist/urls.py
+++ todolist/urls.py
@@ -3,5 +3,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.home,name='home')
+    path('',views.home,name='home'),
+    path('add',views.addTodoItem,name='add'),
+    path('completed\<todo_id>',views.completedTodo,name='completedd'),
+    path('deletecompleted',views.deleteCompleted,name='deletecompleted'),
+    path('deleteall',views.deleteAll,name='deleteall'),
+
+
+
 ]
